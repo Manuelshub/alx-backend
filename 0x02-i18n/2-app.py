5f56d190e055ac0f2ec8c40aabab3e0479e3796d
Emmanuel <kcemmy03@gmail.com>
2024-08-27 23:58:49 +0100

    Added decorator

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,10 +21,12 @@ class Config():
 app.config.from_object(Config)
 
 
+@babel.localeselector
 def get_locale():
     """ get_locale
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route("/", strict_slashes=False)
 def index() -> str:
